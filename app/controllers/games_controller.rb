e8e68faa0b5eabb55ead3285d86edf2bd9f91f11
sulav kharel <kharel.sulav2016@gmail.com>
2019-05-21 11:16:46 +0900

    done with the exercise

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,17 +2,18 @@ require 'open-uri'
 
 class GamesController < ApplicationController
   def new
-    @letters = [*('A'..'Z')].sample(11)
+    @letters = [*('A'..'Z')].sample(10)
   end
 
   def score
     @score = 0
+    @letters = params[:letters]
     @answer = params[:answer]
     @length = @answer.length
     url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
     data_serialized = open(url).read
     data = JSON.parse(data_serialized)
-    data['found']
-    @score = @length * 10 if data['found']
+    value = data['found']
+    @score = @length * 10 if value || @letters.include?(@answer)
   end
 end
